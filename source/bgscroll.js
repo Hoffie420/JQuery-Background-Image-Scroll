c50f3a597286a733dc40d40278341d26f1cfb7d1
Dawid Hofman <dawidhofman@MacBook-Pro-Dawid.local>
2017-12-08 21:34:34 +0100

    Minify code

--- source/bgscroll.js
+++ source/bgscroll.js
@@ -1,21 +1,12 @@
 (function($){
   $.fn.bgscroll = function( options ) {
-    var x = $.extend({
-        bgpositionx: 50,
-        direction: 'bottom',
-        debug: false,
-        min: 0,
-        max: 100
-    }, options );
-    var a=$(document).height() - $(window).height();
-    var b=a-(this.offset().top+this.height());
-    if(this.offset().top<a)b=0;
+    var x=$.extend({bgpositionx:50,direction:"bottom",debug:!1,min:0,max:100},options);
+    var a=$(document).height()-$(window).height(),b=a-(this.offset().top+this.height());
+    this.offset().top<a&&(b=0);
     var c=(this.offset().top+this.height());
     if($(window).scrollTop()>b && $(window).scrollTop()<c){
       var d=($(window).scrollTop()-b)/(c-b)*100;
-      if(x.direction=='top')d=100-d;
-      if(d>x.max)d=x.max;
-      if(d<x.min)d=x.min;
+      "top"==x.direction&&(d=100-d),d>x.max&&(d=x.max),d<x.min&&(d=x.min);
       if(x.debug)console.log('Element background position: '+d+' %');
     }
     return this.css({
